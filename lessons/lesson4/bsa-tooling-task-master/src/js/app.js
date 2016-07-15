9bfd8dde5e3323d4feced0366762f390e5bda506
Bender <bender.d1mon@gmail.com>
2016-07-15 22:50:16 +0300

    lesson 4 new start

--- lessons/lesson4/bsa-tooling-task-master/src/js/app.js
+++
@@ -1,5 +0,0 @@
-define(['secondModule'], function(secondModule) {
-	var jsOutput = document.getElementById('js-output');
-	jsOutput.innerHTML += 'Entry module works!';
-	// call secondModule run function
-});
--- lessons/lesson4/bsa-tooling-task-master/src/js/secondModule.js
+++
@@ -1,7 +0,0 @@
-define(function() {
-	return {
-		run: function(element) {
-			element.innerHTML += '\nSecond module works!';
-		}
-	};
-});
